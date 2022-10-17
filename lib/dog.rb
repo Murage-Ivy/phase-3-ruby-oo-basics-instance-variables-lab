5e4db76090bc117e488dbe1ca09bfbf93f79567b
Murage-Ivy <ivy.murage@student.moringaschool.com>
2022-10-18 00:04:19 +0300

    Completed assignment

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,15 @@
 class Dog
+  # This is the setter method that when called sets the instance variable to the value passed
+  def name=(dog_name)
+    @this_dogs_name = dog_name
+  end
 
+  # This is the getter method that returns the value of the instance variables
+  def name
+    @this_dogs_name
+  end
 end
+
+lessie = Dog.new
+lessie.name = "Liesy"
+puts lessie.name
